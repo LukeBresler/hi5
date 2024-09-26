f33d24ae9050bc79e9635d49afc30dcf328511e8
Luke Bresler <luke@realpixel.co.za>
2024-09-26 12:03:09 +0100

    Initial Commit

---
+++ Hi5/Hi5App.swift
@@ -0,0 +1,17 @@
+//
+//  Hi5App.swift
+//  Hi5
+//
+//  Created by Luke Bresler on 2024/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Hi5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
